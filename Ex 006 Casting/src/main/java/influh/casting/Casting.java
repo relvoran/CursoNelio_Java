6a863c490e1b8407199d20711f3b3083c859b251
Renato von Randow <106645662+relvoran@users.noreply.github.com>
2024-08-10 16:14:26 -0300

    Add files via upload

---
+++ Ex 006 Casting/src/main/java/influh/casting/Casting.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package influh.casting;
+
+/**
+ *
+ * @author Renato
+ */
+public class Casting {
+
+    public static void main(String[] args) {
+        
+        double a;
+        int b;
+        
+       a = 5.0;
+       b = (int) a;  //casting
+        
+        System.out.println("O valor de b é " + b);
+    }
+}
