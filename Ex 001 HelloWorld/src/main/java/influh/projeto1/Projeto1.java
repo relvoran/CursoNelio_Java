2f3d4066e2cb0a2336b22b6995e3267cc277d94b
Renato von Randow <106645662+relvoran@users.noreply.github.com>
2024-08-10 16:12:29 -0300

    Add files via upload

---
+++ Ex 001 HelloWorld/src/main/java/influh/projeto1/Projeto1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package influh.projeto1;
+
+/**
+ *
+ * @author Renato
+ */
+public class Projeto1 {
+
+    public static void main(String[] args) {
+        int idade = 27;
+        String nome = "Maria";
+        double salario = 4000.00;
+        int x = 13;
+                               
+        System.out.println("Olá, Mundo!");
+        System.out.println(x);
+        System.out.printf("%s tem %d anos e recebe o salário de R$ %.2f por mês", nome, idade, salario);
+        
+        
+    }
+}
